ab22a59f219094873c08b706876787d629ff1490
Robert Bjarnason <robert.bjarnason@gmail.com>
2024-02-12 02:06:15 +0000

    Version 5 of webapp

---
+++ webApps/allOurIdeas/src/aoi-app.ts
@@ -0,0 +1,6 @@
+import { customElement, property } from "lit/decorators.js";
+import { YpApp } from "@yrpri/webapp/yp-app/yp-app.js";
+
+@customElement("aoi-app")
+export class AoiApp extends YpApp {
+}
---
+++ webApps/allOurIdeas/ts-out/aoi-app.d.ts
@@ -0,0 +1,4 @@
+import { YpApp } from "@yrpri/webapp/yp-app/yp-app.js";
+export declare class AoiApp extends YpApp {
+}
+//# sourceMappingURL=aoi-app.d.ts.map
