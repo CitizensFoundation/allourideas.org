ce3e8a2e3ff2a4df211e7ab9a68cec8e10c09f74
Robert Bjarnason <robert.bjarnason@gmail.com>
2024-02-16 00:55:46 +0000

    Work

--- api/config/config.js
+++ api/config/config.js
@@ -2,10 +2,10 @@ module.exports = {
     "production": {
         "use_env_variable":"DATABASE_URL",
         "dialect":"postgres",
-        "ssl":true,
+        "ssl":false, /* TODO: Remove before production */
         "dialectOptions":{
             "ssl":{
-                "require":true,
+                "require":false, /* TODO: Remove before production */
                 "rejectUnauthorized": false
             }
         }
