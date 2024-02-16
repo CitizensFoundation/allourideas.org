73b3cf9333dcd526229f81c41e7ecd5014671a3d
Robert Bjarnason <robert.bjarnason@gmail.com>
2024-02-16 01:15:41 +0000

    Work

---
+++ api/ts-out/config/config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    "production": {
+        "use_env_variable":"DATABASE_URL",
+        "dialect":"postgres",
+        "ssl":false, /* TODO: Remove before production */
+        "dialectOptions":{
+            "ssl":{
+                "require":false, /* TODO: Remove before production */
+                "rejectUnauthorized": false
+            }
+        }
+    }
+}
