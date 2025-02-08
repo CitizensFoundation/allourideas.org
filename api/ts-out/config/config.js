f1314c5d24a0601e02ea812a56986ec8044b4764
Robert Bjarnason <robert.bjarnason@gmail.com>
2025-02-08 23:06:58 +0000

    Work on agents

--- api/ts-out/config/config.js
+++ api/ts-out/config/config.js
@@ -1,13 +1,14 @@
 module.exports = {
     "production": {
-        "use_env_variable":"DATABASE_URL",
-        "dialect":"postgres",
-        "ssl":false, /* TODO: Remove before production */
-        "dialectOptions":{
-            "ssl":{
-                "require":false, /* TODO: Remove before production */
+        "use_env_variable": "DATABASE_URL",
+        "dialect": "postgres",
+        "ssl": false, /* TODO: Remove before production */
+        "dialectOptions": {
+            "ssl": {
+                "require": false, /* TODO: Remove before production */
                 "rejectUnauthorized": false
             }
         }
     }
-}
+};
+export {};
